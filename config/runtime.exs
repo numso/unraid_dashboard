6ecda4f2af04549d310d581146349af50e7e7260
Dallin Osmun <dosmun@gmail.com>
2023-04-10 23:58:18 -0600

    set origins

--- config/runtime.exs
+++ config/runtime.exs
@@ -64,6 +64,7 @@ if config_env() == :prod do
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: port
     ],
+    check_origin: ["https://dashy.dallin.pro", "https://dallin.kuali.cloud"],
     secret_key_base: secret_key_base
 
   # ## SSL Support
